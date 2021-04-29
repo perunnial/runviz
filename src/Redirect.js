273c6d16ed649080d86d04b5c2d11c2a6ac6094f
perunnial <jishnu.dave@gmail.com>
2021-04-29 19:10:55 +0530

    get tokens from auth code

--- src/Redirect.js
+++ src/Redirect.js
@@ -1,9 +1,38 @@
 import React from 'react'
 
-export default function Redirect () {
-  return (
-        <div>
-            <h2>Redirect</h2>
-        </div>
-  )
+import { getAuthCodeFromUrl, getTokensFromAuthCode } from './utils'
+
+class Redirect extends React.Component {
+  state= {
+    accessToken: {},
+    refreshToken: {}
+  };
+
+  componentDidMount () {
+    const logIn = async () => {
+      const authCode = getAuthCodeFromUrl(window.location.href)
+      console.log(authCode)
+      const tokenResponse = await getTokensFromAuthCode(authCode)
+      console.log(tokenResponse)
+      this.setState(
+        {
+          accessToken: tokenResponse.access_token,
+          refreshToken: tokenResponse.refresh_token
+        }
+      )
+      console.log(this.state.accessToken)
+      console.log(this.state.refreshToken)
+    }
+    logIn()
+  }
+
+  render () {
+    return (
+      <div>
+          <h2>Redirect</h2>
+      </div>
+    )
+  }
 }
+
+export default Redirect
---
+++ src/utils.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+export const getAuthCodeFromUrl = (url) => {
+  return url.split('&')[1].slice(5)
+}
+
+export const getTokensFromAuthCode = async (authCode) => {
+  try {
+    const REACT_APP_RUNVIZ_CLIENT_ID = process.env.REACT_APP_RUNVIZ_CLIENT_ID
+    const REACT_APP_RUNVIZ_CLIENT_SECRET = process.env.REACT_APP_RUNVIZ_CLIENT_SECRET
+
+    const request = `https://www.strava.com/api/v3/oauth/token?client_id=${REACT_APP_RUNVIZ_CLIENT_ID}&client_secret=${REACT_APP_RUNVIZ_CLIENT_SECRET}&code=${authCode}&grant_type=authorization_code`
+    console.error(request)
+
+    const response = await axios.post(request)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
