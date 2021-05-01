b5b24344727d7a98867439c6a9b13200fdb29223
perunnial <jishnu.dave@gmail.com>
2021-05-01 15:37:19 +0530

    read from redux store

--- src/routes/LoggedIn.js
+++ src/routes/LoggedIn.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
-export default function LoggedIn () {
-  return (
-        <div>
-            <h2>LoggedIn</h2>
-        </div>
-  )
+import store from '../redux/store'
+
+class LoggedIn extends React.Component {
+  componentDidMount () {
+    // store.subscribe(() => {
+    //  console.log(store.getState().tokenResponse.tr.access_token)
+    // })
+    console.log(store.getState().tokenResponse.tr.access_token)
+    console.log(store.getState().tokenResponse.tr.athlete.id)
+  }
+
+  render () {
+    const fetchedTokenResponse = store.getState().tokenResponse.tr
+    return (
+      <div>
+        <h2>LoggedIn</h2>
+        <h3>{fetchedTokenResponse.access_token}</h3>
+        <h3>{fetchedTokenResponse.athlete.id}</h3>
+        <h3>{fetchedTokenResponse.athlete.firstname} {fetchedTokenResponse.athlete.lastname}</h3>
+        <img src={fetchedTokenResponse.athlete.profile} alt="athlete profile"></img>
+      </div>
+    )
+  }
 }
+
+export default LoggedIn
