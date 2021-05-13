bab347fd114bcd8a3b89fec98ab927e9958869c0
perunnial <jishnu.dave@gmail.com>
2021-05-14 00:45:48 +0530

    update 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react'
 export class NotFound extends Component {
   render () {
     return (
-      <div>
-        <h2>NotFound</h2>
+      <div className="page-wrap d-flex flex-row align-items-center">
+          <div className="container">
+              <div className="row justify-content-center">
+                  <div className="col-md-12 text-center">
+                      <span className="display-4 d-block">404</span>
+                      <div className="mb-4 lead">The page you are looking for was not found</div>
+                  </div>
+              </div>
+          </div>
       </div>
     )
   }
